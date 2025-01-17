de79079540362bf5f66a091e8adab04438cd6bfc
Carlos <carlos.mendoza103@alu.ulpgc.es>
2025-01-17 23:43:21 +0000

    Project divided in two java modules for maven.

--- main.tf
+++ main.tf
@@ -154,7 +154,7 @@ resource "aws_instance" "graph_loader" {
 
 
     sudo /opt/maven/bin/mvn clean package
-    sudo java -DHAZELCAST_IP=$HAZELCAST_IP -DNEO4J_USER=$NEO4J_USER -DNEO4J_PASSWORD=$NEO4J_PASSWORD -DNEO4J_URI=$NEO4J_URI -jar target/neo4j-loader-loader.jar
+    sudo java -DHAZELCAST_IP=$HAZELCAST_IP -DNEO4J_USER=$NEO4J_USER -DNEO4J_PASSWORD=$NEO4J_PASSWORD -DNEO4J_URI=$NEO4J_URI -jar Loader/target/loader-1.0.jar
 
     echo "GraphLoader instance ready."
 
@@ -240,7 +240,7 @@ resource "aws_instance" "api_server" {
     echo "NEO4J_URI: $NEO4J_URI"
 
     sudo /opt/maven/bin/mvn clean package
-    sudo java -DNEO4J_URI=$NEO4J_URI -DNEO4J_USER=$NEO4J_USER -DNEO4J_PASSWORD=$NEO4J_PASSWORD -jar target/neo4j-api-api.jar
+    sudo java -DNEO4J_URI=$NEO4J_URI -DNEO4J_USER=$NEO4J_USER -DNEO4J_PASSWORD=$NEO4J_PASSWORD -jar API/target/api-1.0.jar
     echo "API server is running..."
   EOF
 
