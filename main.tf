fd01a74df1b008b0e3bfc450121bb33b95023490
Carlos <carlos.mendoza103@alu.ulpgc.es>
2025-01-17 19:01:17 +0000

    Terraform files modified.

--- main.tf
+++ main.tf
@@ -2,11 +2,26 @@ provider "aws" {
   region = "us-east-1"
 }
 
+variable "hazelcast_ip" {
+  description = "IP of Hazelcast Server"
+  type        = string
+}
+
+variable "neo4j_password" {
+  description = "Password for Neo4j"
+  type        = string
+}
+
+variable "neo4j_user" {
+  description = "User for Neo4j"
+  type        = string
+}
+
 # VPC
 resource "aws_vpc" "neo4j_vpc" {
   cidr_block = "10.0.0.0/16"
   tags = {
-    Name = "DatamartVPC"
+    Name = "neoVPC"
   }
 }
 
@@ -50,6 +65,7 @@ resource "aws_route_table_association" "neo4j_subnet_association" {
 # Security Group
 resource "aws_security_group" "neo4j_sg" {
   name        = "neo4j-sg"
+  vpc_id      = aws_vpc.neo4j_vpc.id
   description = "Allow traffic for Neo4J"
 
   ingress {
@@ -86,24 +102,22 @@ resource "aws_security_group" "neo4j_sg" {
   }
 }
 
-
-# Instancia EC2
-resource "aws_instance" "neo4j_instance" {
-  ami           = "ami-05576a079321f21f8" # Amazon Linux 2 AMI
+resource "aws_instance" "neo4j_server" {
+  ami           = "ami-05576a079321f21f8"
   instance_type = "t2.micro"
   subnet_id     = aws_subnet.neo4j_subnet.id
   key_name      = "vockey"
-  security_group_names = [aws_security_group.neo4j_sg.name]
+  security_groups = [aws_security_group.neo4j_sg.id]
+  iam_instance_profile   = "EMR_EC2_DefaultRole"
 
   user_data = <<-EOF
     #!/bin/bash
     sudo yum update -y
-    sudo yum install -y java-17-amazon-corretto wget
+    sudo yum install -y wget
 
-    # Instalar Neo4J
-    wget -O - https://debian.neo4j.com/neotechnology.gpg.key | sudo apt-key add -
-    echo 'deb https://debian.neo4j.com stable 4.x' | sudo tee /etc/yum.repos.d/neo4j.list
-    sudo yum update -y
+    # Instalar Neo4j usando YUM
+    sudo rpm --import https://debian.neo4j.com/neotechnology.gpg.key
+    sudo bash -c 'echo -e "[neo4j]\nname=Neo4j Yum Repository\nbaseurl=https://yum.neo4j.com/stable\nenabled=1\ngpgcheck=1" > /etc/yum.repos.d/neo4j.repo'
     sudo yum install -y neo4j
 
     # Configurar Neo4J
@@ -116,7 +130,74 @@ resource "aws_instance" "neo4j_instance" {
   EOF
 
   tags = {
-    Name = "Neo4J-Instance"
+    Name = "Neo4J-Server"
+  }
+}
+
+resource "aws_instance" "graph_loader" {
+  ami           = "ami-05576a079321f21f8"
+  instance_type = "t2.micro"
+  subnet_id     = aws_subnet.neo4j_subnet.id
+  key_name      = "vockey"
+  security_groups = [aws_security_group.neo4j_sg.id]
+  iam_instance_profile   = "EMR_EC2_DefaultRole"
+
+  user_data = <<-EOF
+    #!/bin/bash
+    # Configuración de Maven
+    wget https://dlcdn.apache.org/maven/maven-3/3.9.9/binaries/apache-maven-3.9.9-bin.tar.gz
+    sudo tar -xvzf apache-maven-3.9.9-bin.tar.gz -C /opt
+    sudo mv /opt/apache-maven-3.9.9 /opt/maven
+    echo "export M2_HOME=/opt/maven" | sudo tee /etc/profile.d/maven.sh
+    echo "export PATH=\$M2_HOME/bin:\$PATH" | sudo tee -a /etc/profile.d/maven.sh
+    source /etc/profile.d/maven.sh
+
+    # Clonar repositorio Datamart
+    git clone https://github.com/CreamsCode/neo4j-graph-loader /home/ec2-user/graph-loader
+    cd /home/ec2-user/graph-loader
+
+    # Configuración de Variables de Entorno
+    export HAZELCAST_IP="${var.hazelcast_ip}"
+    export NEO4J_USER="${var.neo4j_user}"
+    export NEO4J_PASSWORD="${var.neo4j_password}"
+    export NEO4J_IP="${aws_instance.neo4j_server.public_ip}"
+    export NEO4J_URI="bolt://$NEO4J_IP:7687"
+
+    # Imprimir valores de las variables para depuración
+    echo "HAZELCAST_IP: $HAZELCAST_IP"
+    echo "NEO4J_USER: $NEO4J_USER"
+    echo "NEO4J_PASSWORD: $NEO4J_PASSWORD"
+    echo "NEO4J_IP: $NEO4J_IP"
+    echo "NEO4J_URI: $NEO4J_URI"
+
+
+    /opt/maven/bin/mvn clean package
+    java -jar target/neo4j-loader-1.0-SNAPSHOT.jar
+
+    echo "GraphLoader instance ready."
+
+  EOF
+
+  tags = {
+    Name = "Graph Loader"
+  }
+}
+
+resource "aws_instance" "graph_query" {
+  ami           = "ami-05576a079321f21f8"
+  instance_type = "t2.micro"
+  subnet_id     = aws_subnet.neo4j_subnet.id
+  key_name      = "vockey"
+  security_groups = [aws_security_group.neo4j_sg.id]
+  iam_instance_profile   = "EMR_EC2_DefaultRole"
+
+  user_data = <<-EOF
+    #!/bin/bash
+
+  EOF
+
+  tags = {
+    Name = "Graph Query"
   }
 }
 
--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,15 @@
 output "neo4j_public_ip" {
-  value = aws_instance.neo4j_instance.public_ip
+  value = aws_instance.neo4j_server.public_ip
 }
 
 output "neo4j_uri" {
-  value = "bolt://${aws_instance.neo4j_instance.public_ip}:7687"
+  value = "bolt://${aws_instance.neo4j_server.public_ip}:7687"
+}
+
+output "graph_loader_public_ip" {
+  value = aws_instance.graph_loader
+}
+
+output "graph_query_public_ip" {
+  value = aws_instance.graph_query
 }
